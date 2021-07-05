45d7dd73f7c2627b974d2fd7433d47a367976cc3
Igor (IPcorp) Patievich <ipcorps@gmail.com>
2021-07-06 02:06:02 +0300

    upd gulp

--- gulpfile.js/config/config-bs.js
+++ gulpfile.js/config/config-bs.js
@@ -14,67 +14,67 @@
  */
 
 module.exports = {
-    "ui": {
-        "port": 3001
+    ui: {
+        port: 3001
     },
-    "files": false,
-    "watchEvents": [
+    files: false,
+    watchEvents: [
         "change"
     ],
-    "watch": false,
-    "ignore": [],
-    "single": false,
-    "watchOptions": {
-        "ignoreInitial": true
+    watch: false,
+    ignore: [],
+    single: false,
+    watchOptions: {
+        ignoreInitial: true
     },
-    "server": false,
-    "proxy": {
+    server: false,
+    proxy: {
         target: "http://localhost:3456",
         ws: true
     },
-    "port": 3000,
-    "middleware": false,
-    "serveStatic": [],
-    "ghostMode": {
-        "clicks": true,
-        "scroll": true,
-        "location": true,
-        "forms": {
-            "submit": true,
-            "inputs": true,
-            "toggles": true
+    port: 3000,
+    middleware: false,
+    serveStatic: [],
+    ghostMode: {
+        clicks: true,
+        scroll: true,
+        location: true,
+        forms: {
+            submit: true,
+            inputs: true,
+            toggles: true
         }
     },
-    "logLevel": "info",
-    "logPrefix": "Browsersync",
-    "logConnections": false,
-    "logFileChanges": true,
-    "logSnippet": true,
-    "rewriteRules": [],
-    "open": "local",
-    "browser": "default",
-    "cors": false,
-    "xip": false,
-    "hostnameSuffix": false,
-    "reloadOnRestart": false,
-    "notify": true,
-    "scrollProportionally": true,
-    "scrollThrottle": 0,
-    "scrollRestoreTechnique": "window.name",
-    "scrollElements": [],
-    "scrollElementMapping": [],
-    "reloadDelay": 0,
-    "reloadDebounce": 500,
-    "reloadThrottle": 0,
-    "plugins": [],
-    "injectChanges": false,
-    "startPath": null,
-    "minify": true,
-    "host": null,
-    "localOnly": false,
-    "codeSync": true,
-    "timestamps": true,
-    "clientEvents": [
+    logLevel: "info",
+    logPrefix: "Browsersync",
+    logConnections: false,
+    logFileChanges: true,
+    logSnippet: true,
+    rewriteRules: [],
+    open: "local",
+    browser: "default",
+    cors: false,
+    xip: false,
+    hostnameSuffix: false,
+    reloadOnRestart: false,
+    notify: true,
+    scrollProportionally: true,
+    scrollThrottle: 0,
+    scrollRestoreTechnique: "window.name",
+    scrollElements: [],
+    scrollElementMapping: [],
+    reloadDelay: 0,
+    reloadDebounce: 500,
+    reloadThrottle: 0,
+    plugins: [],
+    injectChanges: false,
+    startPath: null,
+    minify: true,
+    host: null,
+    localOnly: false,
+    codeSync: true,
+    timestamps: true,
+    clientEvents: [
         "scroll",
         "scroll:element",
         "input:text",
@@ -83,30 +83,30 @@ module.exports = {
         "form:reset",
         "click"
     ],
-    "socket": {
-        "socketIoOptions": {
-            "log": false
+    socket: {
+        socketIoOptions: {
+            log: false
         },
-        "socketIoClientConfig": {
-            "reconnectionAttempts": 50
+        socketIoClientConfig: {
+            reconnectionAttempts: 50
         },
-        "path": "/browser-sync/socket.io",
-        "clientPath": "/browser-sync",
-        "namespace": "/browser-sync",
-        "clients": {
-            "heartbeatTimeout": 5000
+        path: "/browser-sync/socket.io",
+        clientPath: "/browser-sync",
+        namespace: "/browser-sync",
+        clients: {
+            heartbeatTimeout: 5000
         }
     },
-    "tagNames": {
-        "less": "link",
-        "scss": "link",
-        "css": "link",
-        "jpg": "img",
-        "jpeg": "img",
-        "png": "img",
-        "svg": "img",
-        "gif": "img",
-        "js": "script"
+    tagNames: {
+        less: "link",
+        scss: "link",
+        css: "link",
+        jpg: "img",
+        jpeg: "img",
+        png: "img",
+        svg: "img",
+        gif: "img",
+        js: "script"
     },
-    "injectNotification": false
+    injectNotification: false
 };
--- gulpfile.js/wp.js
+++ gulpfile.js/wp.js
@@ -1,6 +1,7 @@
 
 // A few "require"
 const gulp = require("gulp"),
+    stream = require("stream"),
     webpackStream = require("webpack-stream"),
     webpack = require("webpack");
 
@@ -12,6 +13,15 @@ exports.change = path => {
     gulp.src(path)                                          // Entry point
         .pipe(webpackStream(wpOpt, webpack))                // Creating a bundle
         .on("error", () => console.log)
+        .pipe(new stream.Transform({
+            objectMode: true,
+            transform(file, _, cb) {
+                const content = file.contents.toString()
+                    .replace("export declare namespace Mono", "declare namespace Mono");
+                file.contents = Buffer.from(content);
+                cb(null, file);
+            }
+        }))
         .pipe(gulp.dest("."));                              // Saving a bundle
 
     // To see something happen

47c35f0cc20a867e1d85907e47c63cf77387944a
Igor (IPcorp) Patievich <ipcorps@gmail.com>
2021-08-08 15:04:55 +0300

    upd

--- gulpfile.js/ts.js
+++ gulpfile.js/ts.js
@@ -15,22 +15,24 @@ const tsProject_C = gulpTypescript.createProject("./tsconfig.json", { module: "E
 
 exports.change = (path, client) => {
 
+    // To see something happen
+    console.log("\x1B[90m%s \x1b[36m%s\x1b[0m", new Date().toLocaleTimeString(), path, "start of processing...");
+
     // TypeScript processing for require
     const tsRes = gulp.src(path)                                                                // Reading the file 
         .pipe(client ? tsProject_C() : tsProject_S())                                           // TypeScript -> JavaScript
         .on("error", console.log);                                                              // For oops caught a mistake 🙀
 
     tsRes.js
-        .pipe(gulpDel)                                                                          // Deleting the specified line during compilation
+        .pipe(gulpDel())                                                                        // Deleting the specified line during compilation
         // .pipe(gulp.dest("."))                                                                // Saving an intermediate file
         .pipe(gulpIf(gSettings.miniTs, gulpTerser()))                                           // Javascript minifier and ... what else you want
         // .pipe(gulpRename({ extname: ".m.js" }))                                              // Output file extension
         .pipe(gulpRename(dir => dir.dirname = dir.dirname.replace("app\\src", "app\\out")))     // Setting the output path
-        .pipe(gulp.dest("."));                                                                  // Saving the file
+        .pipe(gulp.dest("."))                                                                   // Saving the file
+        .on("end", () => console.log("\x1B[90m%s \x1b[36m%s\x1b[0m", new Date().toLocaleTimeString(), path, "ts processing is complete!"));
 
-    // tsRes.dts.pipe(gulp.dest("."));                                                          // Saving the declaration file
-
-    // To see something happen
-    console.log("\x1B[90m%s \x1b[36mTS %s\x1b[0m", new Date().toLocaleTimeString(), path, "processed");
+    // tsRes.dts.pipe(gulp.dest("."))                                                           // Saving the declaration file
+    //     .on("end", () => console.log("\x1B[90m%s \x1b[36m%s\x1b[0m", new Date().toLocaleTimeString(), path, "d.ts processing is complete!"));
 
 }
--- gulpfile.js/wp.js
+++ gulpfile.js/wp.js
@@ -9,12 +9,13 @@ const wpOpt = require("./config/config-wp.js");
 
 exports.change = path => {
 
+    // To see something happen
+    console.log("\x1B[90m%s \x1b[36m%s\x1b[0m", new Date().toLocaleTimeString(), path, "wp start of processing...");
+
     gulp.src(path)                                          // Entry point
         .pipe(webpackStream(wpOpt, webpack))                // Creating a bundle
         .on("error", () => console.log)
-        .pipe(gulp.dest("."));                              // Saving a bundle
-
-    // To see something happen
-    console.log("\x1B[90m%s \x1b[36mWP %s\x1b[0m", new Date().toLocaleTimeString(), path, "processed");
+        .pipe(gulp.dest("."))                               // Saving a bundle
+        .on("end", () => console.log("\x1B[90m%s \x1b[36m%s\x1b[0m", new Date().toLocaleTimeString(), path, "wp processing is complete!"));
 
 }

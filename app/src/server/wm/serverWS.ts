803df733afb728b23c0072ac4873c2e98caef944
Igor (IPcorp) Patievich <ipcorps@gmail.com>
2021-08-24 14:00:36 +0300

    upd

--- app/src/server/wm/serverWS.ts
+++ app/src/server/wm/serverWS.ts
@@ -9,9 +9,11 @@ import mWsUpd from "./wsUpd";
 
 export function create(sHTTP: mHttp.Server, init: (sHTTP: mHttp.Server, sWS: mSocketIO.Server, socket: mSocketIO.Socket) => void) {
     const sWS = new mSocketIO.Server(sHTTP);
-    sWS.on("connection", (socket: mSocketIO.Socket) => {
-        mWsUpd(sWS, socket);
-        init(sHTTP, sWS, socket);
+    sWS.on("connect", (socket: mSocketIO.Socket) => {
+        setTimeout(() => {
+            mWsUpd(sWS, socket);
+            init(sHTTP, sWS, socket);
+        }, 100);
     });
     return sWS;
 }

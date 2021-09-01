791ccbd81ed2321f1100d3da0f407228f6890e20
Igor (IPcorp) Patievich <ipcorps@gmail.com>
2021-09-01 18:36:09 +0300

    upd

--- app/src/server/wm/serverHTTP.ts
+++ app/src/server/wm/serverHTTP.ts
@@ -6,31 +6,38 @@
 import mFs from "fs";
 import mHttpS from "https";
 import mHttp from "http";
+import mURL from "url";
+import mPath from "path";
 
 import * as mAppSettings from "./appSettings";
 import * as mMime from "./mime";
 
 export function create() {
 
+    const fHttps = mAppSettings.settings.https;
+
     let server: typeof mHttpS | typeof mHttp = mHttpS;
     let options: mHttpS.ServerOptions = {
         key: mFs.readFileSync("../server/crt/localhost.d.key"),
         cert: mFs.readFileSync("../server/crt/localhost.crt"),
     };
-    if (!mAppSettings.settings.https) { server = mHttp; options = {}; }
+    if (!fHttps) { server = mHttp; options = {}; }
 
     return server.createServer(options, (req, res) => {
 
         let file: Buffer,
             type: string;
 
-        if (req.url === "/") req.url = "/index.html";
-        const path = process.cwd() + req.url;
+        const url = new mURL.URL(req.url!, (fHttps ? "https" : "http") + "://" + req.headers.host);
+        let pathName = url.pathname;
+
+        if (pathName === "/") pathName = "/index.html";
+        const path = process.cwd() + pathName;
 
         if (mFs.existsSync(path)) {
             file = mFs.readFileSync(path);
-            const ext = path.match(/[^.]*$/)?.[0];
-            type = mMime.mime[ext ? ext : ""] as string;
+            const ext = mPath.extname(path).substr(1);
+            type = mMime.mime[ext]!;
         } else {
             file = Buffer.from("Hmm...something strange has happened &#129300");
             type = "text/html";
@@ -41,6 +48,6 @@ export function create() {
             "Content-Length": file.length,
         }).end(file);
 
-    }).listen(mAppSettings.settings.port);
+    }).listen(process.env["PORT"] || mAppSettings.settings.port);
 
 }
